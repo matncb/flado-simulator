8beaad2fe52b9afca01d1d0eb3e94f59a6c24df4
matncb <matheus.brustelo@gmail.com>
2022-06-28 21:13:54 -0300

    dfdfd

--- Three-Sided Coin.py
+++ Three-Sided Coin.py
@@ -8,25 +8,15 @@ import numpy as np
 
 import pybullet
 import pandas as pd
+import xlsxwriter
 
 # Number of tests after which to stop
 # or enter very large number and end simulation via pressing 'u'
 # Will test every combination of test values TESTS times
-TESTS = 4
-
-# Diameter of coin IN M
-
-RATIO = 0.6
-RADIUS = 0.0075
-
-DIAMETER = 2*RADIUS
-HEIGHT = RATIO * RADIUS
-
-# Mass in gram
-MASS = 2
+TESTS = 2
 
 # Force with which to throw/flip coins
-FORCE = 0
+FORCE = 0.07
 
 #altura de lançamento
 ALTURA = 0.035
@@ -41,8 +31,8 @@ RESTITUTION = 0.3
 
 # Friction
 LATERAL_FRICTION = 0.3
-SPINNING_FRICTION = 0
-ROLLING_FRICTION = 0
+SPINNING_FRICTION = 0.05
+ROLLING_FRICTION = 0.0
 
 # Distance between coins in cm, coins will touch with lower values,
 # might lead to skewed results
@@ -56,7 +46,7 @@ ANGULAR_DAMPING = 0.05
 
 # -- Variables below are valid for all tests -- #
 # Number of objects per test
-OBJECTS = 800
+OBJECTS = 500
 
 # Time steps for physics simulation in seconds
 # Smaller value = more accurate simulation, but more computationally expensive
@@ -68,7 +58,7 @@ SUBSTEPS = 2
 
 # Used to scale up centimeters to decimeters, while keeping accurate physics
 # Bullet physics doesn't work well with objects at cm scale
-SCALE = 10
+SCALE = 1
 
 # Time after which to stop one run of simulation
 # must be high enough for all coins to settle, 
@@ -77,18 +67,21 @@ CUTOFF = 2
 
 # Slow down simulation each step to see what is happening
 # mainly for code refinement/debugging purposes
-SLOWDOWN = 0
+SLOWDOWN = 0.0
 
 SHOW_DEBUG_GUI = False
 
-data = {"Ratio": [RATIO], "Ps": [] }
-side = 0
-heads = 0
-tails = 0
-total = 0
+# Diameter of coin IN M
+
+
+data = {}
 
 # Random distribution of SO(3)
 # see http://planning.cs.uiuc.edu/node198.html
+
+sysRand = random.SystemRandom()
+
+pybullet.connect(pybullet.GUI)
 
 def generateRandomQuaternion():
     u1 = sysRand.random()
@@ -100,99 +93,125 @@ def generateRandomQuaternion():
     z = math.sqrt(u1) * math.cos(2 * math.pi * u3)
     return (x, y, z, w)
 
-sysRand = random.SystemRandom()
 
-pybullet.connect(pybullet.GUI)
+def simulate(ratio):
+    print("")
+    print("Razão: " + str(ratio))
+    print("")
 
-# Set up simulation
-pybullet.setRealTimeSimulation(0)
-pybullet.setTimeStep(STEPSIZE)
-pybullet.resetDebugVisualizerCamera(10, 0, -50, [5, 2, 0])
-pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, SHOW_DEBUG_GUI)
-pybullet.setPhysicsEngineParameter(numSubSteps=SUBSTEPS)
+    RATIO = ratio
+    RADIUS = 0.0075
 
-print("------------------------------------")
-print("Simulation started, press 'u' to end")
-print("------------------------------------")
+    HEIGHT = RATIO * RADIUS
+    DIAMETER = RADIUS*2
 
-for test in range(TESTS):
-    print("Test #" + str(test + 1))
-    stopSim = False
+    # Mass in gram
+    MASS = 0.5487*RATIO + 0.2787
 
-    # Turn off rendering for building world
-    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 0)
+    side = 0
+    heads = 0
+    tails = 0
+    total = 0
 
-    # Remove all objects
-    pybullet.resetSimulation()
-    pybullet.setGravity(0, 0, -9.80665 * SCALE)
+    # Set up simulation
+    pybullet.setRealTimeSimulation(0)
+    pybullet.setTimeStep(STEPSIZE)
+    pybullet.resetDebugVisualizerCamera(10, 0, -50, [5, 2, 0])
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, SHOW_DEBUG_GUI)
+    pybullet.setPhysicsEngineParameter(numSubSteps=SUBSTEPS)
 
-    # Create ground plane
-    pybullet.createCollisionShape(pybullet.GEOM_PLANE)
-    pybullet.createMultiBody(0, 0)
-    pybullet.changeDynamics(0, -1, restitution=RESTITUTION, lateralFriction=LATERAL_FRICTION, spinningFriction=SPINNING_FRICTION, rollingFriction=ROLLING_FRICTION)
+    for test in range(TESTS):
+        print("Test #" + str(test + 1))
+        stopSim = False
 
-    # Create collision shape for coin, which will be used for all bodies
-    colCylinder = pybullet.createCollisionShape(pybullet.GEOM_CYLINDER, radius=RADIUS * SCALE, height= HEIGHT * SCALE)
+        # Turn off rendering for building world
+        pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 0)
 
-    # Create bodies with random forces applied
-    for i in range(OBJECTS):
-        x = pybullet.createMultiBody(baseMass=MASS / 1000 * SCALE, baseCollisionShapeIndex=colCylinder,
-                                basePosition=[i % 10 / 100 * DISTANCE_BETWEEN_COINS * SCALE, i / 10 / 100 * DISTANCE_BETWEEN_COINS * SCALE, ALTURA * SCALE], 
-                                baseOrientation=generateRandomQuaternion())
+        # Remove all objects
+        pybullet.resetSimulation()
+        pybullet.setGravity(0, 0, -9.80665 * SCALE)
 
-        pybullet.changeDynamics(x, -1, linearDamping=LINEAR_DAMPING, angularDamping=ANGULAR_DAMPING, restitution=RESTITUTION,
-                                lateralFriction=LATERAL_FRICTION, spinningFriction=SPINNING_FRICTION, rollingFriction=ROLLING_FRICTION)
+        # Create ground plane
+        pybullet.createCollisionShape(pybullet.GEOM_PLANE)
+        pybullet.createMultiBody(0, 0)
+        pybullet.changeDynamics(0, -1, restitution=RESTITUTION, lateralFriction=LATERAL_FRICTION, spinningFriction=SPINNING_FRICTION, rollingFriction=ROLLING_FRICTION)
 
-        pybullet.applyExternalForce(x, -1, [(sysRand.random() * 2 * FORCE - FORCE) * SCALE / STEPSIZE * FORCE_APPLICATION_TIME,
-                                            (sysRand.random() * 2 * FORCE - FORCE) * SCALE / STEPSIZE * FORCE_APPLICATION_TIME,
-                                            (sysRand.random() * 2 * FORCE - FORCE) * SCALE / STEPSIZE * FORCE_APPLICATION_TIME],
-                                [(sysRand.random() * DIAMETER - DIAMETER / 2) / 100 * SCALE*0,
-                                (sysRand.random() * DIAMETER - DIAMETER / 2) / 100 * SCALE*0,
-                                (sysRand.random() * DIAMETER - DIAMETER / 2) / 100 * SCALE*0],
-                                pybullet.LINK_FRAME)
+        # Create collision shape for coin, which will be used for all bodies
+        colCylinder = pybullet.createCollisionShape(pybullet.GEOM_CYLINDER, radius=RADIUS * SCALE, height= HEIGHT * SCALE)
 
-        
+        # Create bodies with random forces applied
+        for i in range(OBJECTS):
+            x = pybullet.createMultiBody(baseMass= MASS / 1000 * SCALE, baseCollisionShapeIndex=colCylinder,
+                                    basePosition=[i % 10 / 100 * DISTANCE_BETWEEN_COINS * SCALE, i / 10 / 100 * DISTANCE_BETWEEN_COINS * SCALE, ALTURA * SCALE], 
+                                    baseOrientation=generateRandomQuaternion())
 
-    # Turn on rendering again
-    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 1)
+            pybullet.changeDynamics(x, -1, linearDamping=LINEAR_DAMPING, angularDamping=ANGULAR_DAMPING, restitution=RESTITUTION,
+                                    lateralFriction=LATERAL_FRICTION, spinningFriction=SPINNING_FRICTION, rollingFriction=ROLLING_FRICTION)
 
-    for i in range(int(1/STEPSIZE * CUTOFF)):
-        pybullet.stepSimulation()
-        time.sleep(SLOWDOWN)
-        keys = pybullet.getKeyboardEvents()
-        for k in keys:
-            if(k == 117):
-                if not stopSim:
-                    stopSim = True
-                    print("Will exit after this test finished")
+            pybullet.applyExternalForce(x, -1, [(sysRand.random() * 2 * FORCE - FORCE) * SCALE / STEPSIZE * FORCE_APPLICATION_TIME,
+                                                (sysRand.random() * 2 * FORCE - FORCE) * SCALE / STEPSIZE * FORCE_APPLICATION_TIME,
+                                                (sysRand.random() * 2 * FORCE - FORCE) * SCALE / STEPSIZE * FORCE_APPLICATION_TIME],
+                                    [(sysRand.random() * DIAMETER - DIAMETER / 2) / 100 * SCALE*0,
+                                    (sysRand.random() * DIAMETER - DIAMETER / 2) / 100 * SCALE*0,
+                                    (sysRand.random() * DIAMETER - DIAMETER / 2) / 100 * SCALE*0],
+                                    pybullet.LINK_FRAME)
 
-    # First object is ground plane -> ignore
-    for i in range(1, OBJECTS + 1):
-        pos, angle = pybullet.getBasePositionAndOrientation(i)
-        angle = pybullet.getEulerFromQuaternion(angle)
-        total += 1
-        # Check how coin landed:
-        angle = (angle[0] + 2 * math.pi) % (2 * math.pi)
+            
 
-        # Side
-        if math.pi / 2 - (math.pi / 2 - 1.3) <= angle <= math.pi / 2 + (math.pi / 2 - 1.3) or math.pi / 2 * 3 - (math.pi / 2 - 1.3) <= angle <= math.pi / 2 * 3 + (math.pi / 2 - 1.3):
-            side += 1
+        # Turn on rendering again
+        pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 1)
 
-        # Heads
-        if angle <= 1.3 or 2 * math.pi - 1.3 <= angle:
-            heads += 1
+        for i in range(int(1/STEPSIZE * CUTOFF)):
+            pybullet.stepSimulation()
+            time.sleep(SLOWDOWN)
+            keys = pybullet.getKeyboardEvents()
+            for k in keys:
+                if(k == 117):
+                    if not stopSim:
+                        stopSim = True
+                        print("Will exit after this test finished")
 
-        # Tails
-        if math.pi - 1.3 <= angle <= math.pi + 1.3:
-            tails += 1
+        # First object is ground plane -> ignore
+        for i in range(1, OBJECTS + 1):
+            pos, angle = pybullet.getBasePositionAndOrientation(i)
+            angle = pybullet.getEulerFromQuaternion(angle)
+            total += 1
+            # Check how coin landed:
+            angle = (angle[0] + 2 * math.pi) % (2 * math.pi)
 
-    if(stopSim):
-        break
+            # Side
+            if math.pi / 2 - (math.pi / 2 - 1.3) <= angle <= math.pi / 2 + (math.pi / 2 - 1.3) or math.pi / 2 * 3 - (math.pi / 2 - 1.3) <= angle <= math.pi / 2 * 3 + (math.pi / 2 - 1.3):
+                side += 1
 
-print("Lado: " + str(side))
-print("Face 1: " + str(heads))
-print("Face 2: " + str(tails))
-print("Total : " + str(total))
+            # Heads
+            if angle <= 1.3 or 2 * math.pi - 1.3 <= angle:
+                heads += 1
+
+            # Tails
+            if math.pi - 1.3 <= angle <= math.pi + 1.3:
+                tails += 1
+
+        if(stopSim):
+            break
+
+    Ps = side/total
+    data[RATIO] = Ps
+    return Ps
+
+
+razoes = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5]
+
+for i in razoes:
+    simulate(i)
+
 print("")
-data["Ps"] = side/total
-print("Probabilidade: " + str(data['Ps']))
+print(data)
+
+workbook = xlsxwriter.Workbook('simulacoes/teste3.xlsx')
+worksheet = workbook.add_worksheet()
+
+for i in range(len(razoes)):
+    worksheet.write(i, 0, razoes[i])
+    worksheet.write(i, 1, data[razoes[i]])
+
+workbook.close()
